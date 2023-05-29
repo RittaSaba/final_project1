725f127903baa806e6eaae40cf745b9cc5268fb4
LENOVO <rittasaba3@gmail.com>
2023-05-29 04:35:07 -0700

    firsst

--- lib/features/attachments/data/dataSources/attachment_local_data_source.dart
+++ lib/features/attachments/data/dataSources/attachment_local_data_source.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 
-
 import 'package:dartz/dartz.dart';
-import 'package:final_project1/features/attachments/domain/entities/attachment.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../core/error/exception.dart';
@@ -10,6 +8,7 @@ import '../models/attachment_model.dart';
 
 abstract class AttachmentLocalDataSource {
   Future<List<AttachmentModel>> getCachedAttachments();
+
   Future<Unit> cacheAttachments(List<AttachmentModel> AttachmentModels);
 }
 
@@ -17,21 +16,19 @@ const CACHED_ATTACHMENTS = "CACHED_ATTACHMENTS";
 
 class AttachmentLocalDataSourceImpl implements AttachmentLocalDataSource {
   final SharedPreferences sharedPreferences;
-  AttachmentLocalDataSourceImpl({required this.sharedPreferences});
 
+  AttachmentLocalDataSourceImpl({required this.sharedPreferences});
 
   @override
   Future<Unit> cacheAttachments(List<AttachmentModel> attachmentModels) {
     List attachmenttModelsToJson = attachmentModels
-        .map<Map<String, dynamic>>((attachmentModels) => attachmentModels.toJson())
+        .map<Map<String, dynamic>>(
+            (attachmentModels) => attachmentModels.toJson())
         .toList();
-    sharedPreferences.setString(CACHED_ATTACHMENTS, json.encode(attachmenttModelsToJson));
+    sharedPreferences.setString(
+        CACHED_ATTACHMENTS, json.encode(attachmenttModelsToJson));
     return Future.value(unit);
   }
-
-
-
-
 
   @override
   Future<List<AttachmentModel>> getCachedAttachments() {
@@ -39,10 +36,12 @@ class AttachmentLocalDataSourceImpl implements AttachmentLocalDataSource {
     if (jsonString != null) {
       List decodeJsonData = json.decode(jsonString);
       List<AttachmentModel> jsonToAttachmentModels = decodeJsonData
-          .map<AttachmentModel>((jsonAttachmentModel) => AttachmentModel.fromJson(jsonAttachmentModel))
+          .map<AttachmentModel>((jsonAttachmentModel) =>
+              AttachmentModel.fromJson(jsonAttachmentModel))
           .toList();
       return Future.value(jsonToAttachmentModels);
     } else {
       throw EmptyCacheException();
     }
-  }}
+  }
+}
